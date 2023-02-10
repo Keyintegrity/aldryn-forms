b879117aa68b7c1e23f9704b58f2bc3c9ee75747
Artyom Opanchuk <subzero666@list.ru>
2023-02-10 20:31:22 +0300

    Add support for Django 3.2

--- setup.py
+++ setup.py
@@ -2,22 +2,19 @@ from setuptools import find_packages, setup
 
 from aldryn_forms import __version__
 
-
 REQUIREMENTS = [
-    'django>=2.0',
-    'aldryn-boilerplates>=0.7.5',
-    'django-cms>=3.5',
+    'django>=2.2,<4.0',
+    'aldryn-boilerplates>=0.8,<1.0',
+    'django-cms>=3.7,<4.0',
     'django-emailit',
     'djangocms-text-ckeditor',
-    'djangocms-attributes-field>=1.0.0',
+    'djangocms-attributes-field>=1.1,<3.0',
     'django-tablib',
     'tablib',
     'pillow',
     'django-filer',
     'django-sizefield',
-    'six>=1.0',
 ]
-
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
@@ -27,17 +24,22 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Framework :: Django',
     'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django :: 3.1',
+    'Framework :: Django :: 3.2',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries',
 ]
-
 
 setup(
     name='aldryn-forms',
